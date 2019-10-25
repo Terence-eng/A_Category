5bb6c8acee6c03189867d0bb03dd80c159fe3034
chenweixin <chenweixin@turingcat.com>
2019-10-25 17:07:54 +0800

    Initial commit

---
+++ Example/A_Category/GTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GTAppDelegate.h
+//  A_Category
+//
+//  Created by Jamie-Ling-iOS on 10/25/2019.
+//  Copyright (c) 2019 Jamie-Ling-iOS. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/A_Category/GTViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTViewController.h
+//  A_Category
+//
+//  Created by Jamie-Ling-iOS on 10/25/2019.
+//  Copyright (c) 2019 Jamie-Ling-iOS. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GTViewController : UIViewController
+
+@end
